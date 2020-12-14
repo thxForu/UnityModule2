ed7b2923d3c9a1726ac72cdcbca2c4ac426a55f1
thx4u <xavavka12345@gmail.com>
2020-12-14 10:15:57 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speedFraction = 1f;
+    
+    private Camera cam;
+    private Vector3 posToMove;
+    private Mover _mover;
+
+    void Start()
+    {
+        cam = Camera.main;
+        _mover = GetComponent<Mover>();
+    }
+    void Update()
+    {
+        InteractWithMovement();
+    }
+
+    private void InteractWithMovement()
+    {
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+        if (hasHit)
+        { 
+            if (Input.GetMouseButtonDown(0))
+            {
+                _mover.MoveTo(hit.point, speedFraction);
+            }
+        }
+    }
+    private Ray GetMouseRay()
+    {
+        return cam.ScreenPointToRay(Input.mousePosition);
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject enemyPrefab;
+    private Vector3 terrain;
+    private void Start()
+    {
+        
+        enemyPrefab.GetComponent<BoxCollider>();
+        terrain = GameObject.Find("Terrain").GetComponent<Terrain>().terrainData.size/2;
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Instantiate(enemyPrefab,
+                new Vector3(Random.Range(-terrain.x, terrain.x), 0.5f, Random.Range(-terrain.z, terrain.z)), Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
